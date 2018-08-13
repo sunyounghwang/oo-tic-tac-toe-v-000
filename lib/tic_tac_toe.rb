59237a88873a17f473e2f53ac72f48a01ead9192
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-08-13 21:28:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,4 +14,10 @@ class TicTacToe
     [2, 4, 6],
   ]
 
-end
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
